0b730e80d6d437a7d53b000168d81deb2e26f104
minwichita <59030206@kmitl.ac.th>
2017-09-16 12:16:45 +0700

    edmin-02pr

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -5,12 +5,12 @@ class Program
 {
     static void Main()
     {
-        sayHello();
+        sayHello("Hello World.");
         Console.ReadLine();
     }
 
-    void sayHello()
+    static void sayHello(string str)
     {
-        Console.WriteLine("Hello World.");
+        Console.WriteLine(str);
     }
 }
