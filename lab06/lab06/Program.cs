c73d6b8e4b4f326188d7d26c3a240e5027683ef0
minwichita <59030206@kmitl.ac.th>
2017-09-16 13:45:02 +0700

    edmin-04pr

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -5,12 +5,17 @@ class Program
 {
     static void Main()
     {
-        sayHello();
+        int b = add(1, 2, 3, 4, 5);
+        Console.WriteLine("b = {0}", b);
+        Console.WriteLine("1+5+8+4+1+6+6+8 = " + add(1, 5, 8, 4, 1, 6, 6, 8));
         Console.ReadLine();
     }
 
-    void sayHello()
+    static int add(params int[] i)
     {
-        Console.WriteLine("Hello World.");
+        int sum = 0; ;
+        foreach (int j in i)
+            sum += j;
+        return sum;
     }
 }
