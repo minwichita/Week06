9b16129bdf3b6e9681b5f24af82911e30c27093f
minwichita <59030206@kmitl.ac.th>
2017-09-21 20:46:49 +0700

    edmin-24

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -5,12 +5,12 @@ class Program
 {
     static void Main()
     {
-        sayHello();
-        Console.ReadLine();
-    }
 
-    void sayHello()
-    {
-        Console.WriteLine("Hello World.");
+        int a = 2;
+        float f = 3.5f;
+        double d = 4.99d;
+        decimal money = 1254841354;
+        Console.WriteLine(a + " " + f + " " + d + " " + money);
+
     }
 }
