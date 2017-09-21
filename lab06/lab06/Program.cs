cbf331fdc47a3e2137267c2b06d961fbc16bfbfe
minwichita <59030206@kmitl.ac.th>
2017-09-21 20:59:24 +0700

    edmin-28

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -1,16 +1,31 @@
-﻿
-using System;
+﻿using System;
+using System.Net;  // for web client
+using System.IO;   // for stream writer
 
-class Program
+
+namespace Labweek06
 {
-    static void Main()
+    class Program
     {
-        sayHello();
-        Console.ReadLine();
-    }
+        static void Main(string[] args)
+        {
+            double w = 0d;
+            ask(ref w);
+            calculate(ref w);
+            Console.WriteLine("Weight on Moon is : {0}", w);
+            Console.ReadKey();
+        }
+        static double ask(ref double w)
+        {
+            Console.Write("Enter weight on Moon : ");
+            w = double.Parse(Console.ReadLine());
+            return w;
 
-    void sayHello()
-    {
-        Console.WriteLine("Hello World.");
+        }
+        static double calculate(ref double w)
+        {
+            w *= 1.6;
+            return w;
+        }
     }
 }
