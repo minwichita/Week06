b0afc654f634bb747a8f3dd9b182594dee022ab1
minwichita <59030206@kmitl.ac.th>
2017-09-21 20:04:57 +0700

    edmin-10

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -2,15 +2,23 @@
 using System;
 
 class Program
-{
-    static void Main()
     {
-        sayHello();
-        Console.ReadLine();
-    }
+        static void Main()
+        {
+            int x = 17;
+            double y = x / 4.0;
+            double z = y * y;
+            Console.WriteLine(y);
+            Console.WriteLine(z);
 
-    void sayHello()
-    {
-        Console.WriteLine("Hello World.");
+            int a = 5;
+            int b = 4;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + (b++));
+            Console.WriteLine(a + b);
+            Console.WriteLine(a + (++b));
+            Console.WriteLine(a + b);
+            Console.WriteLine(14 / a);
+            Console.WriteLine(14 % a);
+        }
     }
-}
