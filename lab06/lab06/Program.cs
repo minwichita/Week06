de78d1c370f58a511d2ef1a6cd2ff317e21ebd47
minwichita <59030206@kmitl.ac.th>
2017-09-21 20:32:31 +0700

    edmin-18

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -5,12 +5,19 @@ class Program
 {
     static void Main()
     {
-        sayHello();
-        Console.ReadLine();
-    }
 
-    void sayHello()
-    {
-        Console.WriteLine("Hello World.");
+        double myDouble = 9.9d;
+        Console.WriteLine(myDouble);
+
+        long myLong = (long)myDouble;
+        Console.WriteLine(myLong);
+
+        myDouble = 9e9d;
+        Console.WriteLine(myDouble);
+
+        int myInt = (int)myDouble;
+        Console.WriteLine(myInt);
+        Console.WriteLine(int.MinValue);
+
     }
 }
