67c11510cc806e1c9387eb05c5b658447bf52c9a
minwichita <59030206@kmitl.ac.th>
2017-09-21 20:56:58 +0700

    edmin-27

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -1,16 +1,28 @@
-﻿
-using System;
+﻿using System;
+using System.Net;  // for web client
+using System.IO;   // for stream writer
 
-class Program
+
+namespace Labweek06
 {
-    static void Main()
+    class Program
     {
-        sayHello();
-        Console.ReadLine();
-    }
-
-    void sayHello()
-    {
-        Console.WriteLine("Hello World.");
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int H, L1, L2;
+            Console.Write("Enter height : ");
+            H = int.Parse(Console.ReadLine());
+            Console.Write("Enter long1 : ");
+            L1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter long2 : ");
+            L2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Trapezoid Area is : " + ans(H, L1, L2));
+            Console.ReadKey();
+        }
+        static double ans(int H, int L1, int L2)
+        {
+            return (0.5 * (L1 + L2) * H);
+        }
     }
 }
