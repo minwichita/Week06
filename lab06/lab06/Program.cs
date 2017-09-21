a411e34b1ac4fe41364cabad0d1174b6883b2534
minwichita <59030206@kmitl.ac.th>
2017-09-21 20:29:14 +0700

    edmin-16

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -5,12 +5,12 @@ class Program
 {
     static void Main()
     {
-        sayHello();
-        Console.ReadLine();
-    }
+        string hello = "Hello";
+        string world = "World";
+        string helloWorld = hello + world;
+        Console.WriteLine(helloWorld);
+        string helloWorld555 = helloWorld + " " + 555;
+        Console.WriteLine(helloWorld555);
 
-    void sayHello()
-    {
-        Console.WriteLine("Hello World.");
     }
 }
