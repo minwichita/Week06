388232d98655eb907b2a2bdc987c22d49990f328
minwichita <59030206@kmitl.ac.th>
2017-09-21 20:50:31 +0700

    edmin-25

--- lab06/lab06/Program.cs
+++ lab06/lab06/Program.cs
@@ -1,16 +1,31 @@
-﻿
-using System;
+﻿using System;
+using System.Net;  // for web client
+using System.IO;   // for stream writer
 
-class Program
+
+namespace Labweek06
 {
-    static void Main()
+    class Program
     {
-        sayHello();
-        Console.ReadLine();
-    }
-
-    void sayHello()
-    {
-        Console.WriteLine("Hello World.");
+        static void Main(string[] args)
+        {
+            int n = 0;
+            ask(ref n);
+            answer(n);
+            Console.ReadKey();
+        }
+        static int ask(ref int n)
+        {
+            Console.Write("Enter number : ");
+            n = int.Parse(Console.ReadLine());
+            return n;
+        }
+        static int answer(int n)
+        {
+            Console.WriteLine("This number is : " + (n % 2 > 0 ? "Odd number" : "Even number"));
+            return n;
+        }
     }
 }
+
+
